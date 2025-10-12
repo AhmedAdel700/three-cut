89f21295ddb15c3b06de10b92ea6bafc8751156d
AhmedAdel700 <ahmedadel.engineer1@gmail.com>
2025-10-12 15:39:36 +0300

    added pdf button

--- src/components/product-detail-page.tsx
+++ src/components/product-detail-page.tsx
@@ -243,24 +243,31 @@ export function ProductDetailPage({ product }: ProductDetailPageProps) {
               )}
 
               {activeTab === "specifications" && (
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-                  {product.specifications.map((specSet: Record<string, string>, index: number) => (
-                    <div key={index}>
-                      <h2 className="col-span-1 md:col-span-2 text-3xl text-white/60 mt-6 first:mt-0 capitaize">
-                        Specifications Set {index + 1}
-                      </h2>
-                      {Object.entries(specSet).map(([key, value]) => (
-                        <div
-                          key={key}
-                          className="flex justify-between items-center py-5 border-b border-border/50"
-                        >
-                          <span className="font-medium">{key}</span>
-                          <span className="text-muted-foreground">{value}</span>
+                <>
+                  <Button className="mb-4 w-full sm:w-fit">Download PDF</Button>
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+                    {product.specifications.map(
+                      (specSet: Record<string, string>, index: number) => (
+                        <div key={index}>
+                          <h2 className="col-span-1 md:col-span-2 text-3xl text-white/60 mt-6 first:mt-0 capitaize">
+                            Specifications Set {index + 1}
+                          </h2>
+                          {Object.entries(specSet).map(([key, value]) => (
+                            <div
+                              key={key}
+                              className="flex justify-between items-center py-5 border-b border-border/50"
+                            >
+                              <span className="font-medium">{key}</span>
+                              <span className="text-muted-foreground">
+                                {value}
+                              </span>
+                            </div>
+                          ))}
                         </div>
-                      ))}
-                    </div>
-                  ))}
-                </div>
+                      )
+                    )}
+                  </div>
+                </>
               )}
 
               {activeTab === "applications" && (
